5a6bb0303bdb5616b604c5c526594bdaf4d056a9
Razvan1990 <53650135+Razvan1990@users.noreply.github.com>
2021-07-15 08:57:48 +0300

    Update DecideEquality.java

--- src/main/java/winner/DecideEquality.java
+++ src/main/java/winner/DecideEquality.java
@@ -21,7 +21,24 @@ public class DecideEquality {
         }
         return result;
     }
+    public int checkValueCardsInCardGameList(List<Integer> player1, List<Integer> player2) {
+        int result = 0;
+        for (int i = player1.size() - 1; i >= 0; ) {
+            for (int j = player2.size() - 1; j >= 0; ) {
+                if (player1.get(i) > player2.get(j)) {
+                    return 1;
+                } else if (player1.get(i) < player2.get(j)) {
+                    return 2;
+                } else {
+                    i--;
+                    j--;
+                }
+            }
+        }
+        return result;
+    }
 }
+
 
 
 
